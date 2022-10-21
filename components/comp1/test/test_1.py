f72736a2a30ecff7887248e5dd81a3dbde0680a0
kenji.tachibana <kenji.tachibana@zozo.com>
2022-10-21 21:36:43 +0900

    change

---
+++ components/comp1/test/test_1.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+def test_sub():
+    expected = 2
+    assert 3 - 1 == expected
---
+++ components/comp2/test/test_2.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+def test_add():
+    expected = 2
+    assert 1 + 1 == expected
